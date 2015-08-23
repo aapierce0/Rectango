a2013591ab88ef021638336df75ee25e36c08182
Avery Pierce <avery.pierce@rrd.com>
2015-08-23 17:32:15 -0500

    Correct some issues from a previously botched merge

--- Digidex Pro/DPBarcodeScannerViewController.m
+++ Digidex Pro/DPBarcodeScannerViewController.m
@@ -86,6 +86,7 @@
     con.videoOrientation = [self videoOrientationForCurrentDeviceOrientation];
     
     [self.scannerView.layer insertSublayer:self.preview atIndex:0];
+    self.scannerView.layer.masksToBounds = YES;
     
     [self.session startRunning];
 	return YES;
@@ -217,13 +218,14 @@
 		_cancelScanView.center = CGPointMake(_cancelScanView.bounds.size.width/2, self.scrollView.bounds.size.height + (_cancelScanView.bounds.size.height/2));
 		
 		_cancelScanView.layer.masksToBounds = NO;
-		_cancelScanView.layer.shadowOffset = CGSizeMake(0, 1.0);
-		_cancelScanView.layer.shadowRadius = 3.0;
+		_cancelScanView.layer.shadowOffset = CGSizeMake(0, -1.0);
+		_cancelScanView.layer.shadowRadius = 5.0;
 		_cancelScanView.layer.shadowOpacity = 0.2;
 		
 		UILabel *cancelScanLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 20)];
 		cancelScanLabel.text = @"Cancel";
-		cancelScanLabel.textColor = [UIColor blueColor];
+        cancelScanLabel.font = [UIFont systemFontOfSize:20 weight:UIFontWeightBold];
+		cancelScanLabel.textColor = self.view.tintColor;
 		cancelScanLabel.translatesAutoresizingMaskIntoConstraints = NO;
 		
 		[_cancelScanView addSubview:cancelScanLabel];
@@ -254,18 +256,25 @@
 		[self.scrollView setNeedsUpdateConstraints];
 		
 		self.backButton.enabled = NO;
+        self.preview.opacity = 0.0;
+        
+        self.scrollView.scrollEnabled = NO;
 		
 		// Animate the scanner into the full screen
 		[UIView animateWithDuration:0.4 animations:^{
 			
 			[self.scrollView layoutIfNeeded];
+            self.preview.frame = self.scannerView.bounds;
+            self.preview.opacity = 1.0;
 			
 			// Fade out the auxillery controls
 			self.auxView.alpha = 0.0;
 			self.backButton.alpha = 0.0;
+            self.tapToScanLabel.alpha = 0.0;
 			
 			// Move the cancel view into frame
 			_cancelScanView.center = CGPointMake(_cancelScanView.bounds.size.width/2, self.scrollView.bounds.size.height - (_cancelScanView.bounds.size.height/2));
+			_cancelScanView.layer.shadowOpacity = 0.6;
 			
 		} completion:^(BOOL finished) {
 			
@@ -291,6 +300,8 @@
 		// Fade out the auxillery controls
 		self.auxView.alpha = 1.0;
 		self.backButton.alpha = 1.0;
+        self.tapToScanLabel.alpha = 1.0;
+        self.preview.opacity = 0.0;
 		
 		// Move the cancel view into frame
 		_cancelScanView.center = CGPointMake(_cancelScanView.bounds.size.width/2, self.scrollView.bounds.size.height + (_cancelScanView.bounds.size.height/2));
@@ -303,6 +314,9 @@
 		[_cancelScanView removeFromSuperview];
 		_cancelScanView = nil;
 		_scannerIsShown = NO;
+        
+        [self.preview removeFromSuperlayer];
+        self.scrollView.scrollEnabled = YES;
 		
 	}];
 }
